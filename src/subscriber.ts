ae52f242d4153d838b8464c424f0d1a051d27de5
Harro Lissenberg <harro@Harros-MacBook-Pro.local>
2016-05-17 13:31:01 +0200

    Log all grid events

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -5,20 +5,29 @@ import {customElement, noView} from 'aurelia-framework';
 @noView()
 export class Subscriber {
 
-  private subscription: Subscription;
+  private channels: string[] = ["au-grid:row:selected", "au-grid:column:sorted"];
+
+  private subscriptions: Subscription[] = [];
   
   constructor(private eventAggregator: EventAggregator) {
   }
 
 
   attached() {
-    this.subscription = this.eventAggregator.subscribe("au-grid:row:selected", data => {
-      console.log("Click event observed: " + data.column1);
-    });
+    for(let channel of this.channels) {
+      this.subscriptions.push(
+        this.eventAggregator.subscribe(channel, data => {
+          console.log(`Event observed on ${channel}`);
+          console.log(data);
+        })
+      );
+    }
   }
 
   detached() {
-    this.subscription.dispose();
+    for(let sub of this.subscriptions) {
+      sub.dispose();
+    }
   }
   
   
